9da83146154b8f9ed69657fc422801a3b672c514
chris.smith <chris.smith@zocdoc.com>
2018-07-03 12:05:47 -0400

    add repo api

---
+++ repo.go
@@ -0,0 +1,31 @@
+package sentry
+
+import (
+	"fmt"
+	"time"
+)
+
+type Repo struct {
+	Id          string        `json:"id,omitempty"`
+	Status      string        `json:"status,omitempty"`
+	Name        string        `json:"name,omitempty"`
+	Url         string        `json:"url,omitempty"`
+	Provider    *RepoProvider `json:"provider,omitempty"`
+	DateCreated *time.Time    `json:"dateCreated,omitempty"`
+}
+
+type RepoProvider struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (c *Client) CreateRepo(o Organization, repo Repo) error {
+	err := c.do("POST", fmt.Sprintf("organizations/%s/repos", *o.Slug), nil, &repo)
+	return err
+}
+
+func (c *Client) GetRepos(o Organization) ([]Repo, error) {
+	repos := make([]Repo, 0)
+	err := c.do("GET", fmt.Sprintf("organizations/%s/repos", *o.Slug), &repos, nil)
+	return repos, err
+}
